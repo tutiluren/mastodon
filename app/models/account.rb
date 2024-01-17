bd394a5cb7e9abd3dc2f8e8d94c450f939797ea2
tutiluren <150017299+tutiluren@users.noreply.github.com>
2024-01-17 15:37:34 +0100

    Marking the site for emoji domain support in mentions

--- app/models/account.rb
+++ app/models/account.rb
@@ -65,8 +65,9 @@ class Account < ApplicationRecord
 
   BACKGROUND_REFRESH_INTERVAL = 1.week.freeze
 
+  EMOJIS        = emoji regexp goes here
   USERNAME_RE   = /[a-z0-9_]+([a-z0-9_.-]+[a-z0-9_]+)?/i
-  MENTION_RE    = %r{(?<![=/[:word:]])@((#{USERNAME_RE})(?:@[[:word:].-]+[[:word:]]+)?)}i
+  MENTION_RE    = %r{(?<![=/[:word:]])@((#{USERNAME_RE})(?:@[[:word:].-#{EMOJIS}]+[[:word:]]+)?)}i
   URL_PREFIX_RE = %r{\Ahttp(s?)://[^/]+}
   USERNAME_ONLY_RE = /\A#{USERNAME_RE}\z/i
 
